8825571b9d74e7f63bf1c6076cb8d7416cbb10ab
Park Junoh <qkrwnsdh.0@gmail.com>
2024-05-22 19:58:48 +0900

    version1
    채팅 가능 구현

--- Client.py
+++ Client.py
@@ -1,12 +1,33 @@
 from socket import *
+import threading
+import time
+
+
+def send(sock):
+    while True:
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print('상대방 :', recvData.decode('utf-8'))
+
+
+port = 8081
 
 clientSock = socket(AF_INET, SOCK_STREAM)
-clientSock.connect(('127.0.0.1', 8080))
+clientSock.connect(('127.0.0.1', port))
 
-print('연결 확인 됐습니다.')
-clientSock.send('I am a client'.encode('utf-8'))
+print('접속 완료')
 
-print('메시지를 전송했습니다.')
+sender = threading.Thread(target=send, args=(clientSock,))
+receiver = threading.Thread(target=receive, args=(clientSock,))
 
-data = clientSock.recv(1024)
-print('받은 데이터 : ', data.decode('utf-8'))
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
--- Sever.py
+++ Sever.py
@@ -1,15 +1,38 @@
 from socket import *
+import threading
+import time
+
+
+def send(sock):
+    while True:
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print('상대방 :', recvData.decode('utf-8'))
+
+
+port = 8081
 
 serverSock = socket(AF_INET, SOCK_STREAM)
-serverSock.bind(('', 8080))
-serverSock.listen(1)
+serverSock.bind(('', port))
+serverSock.listen()
+
+print('%d번 포트로 접속 대기중...'%port)
 
 connectionSock, addr = serverSock.accept()
 
-print(str(addr),'에서 접속이 확인되었습니다.')
+print(str(addr), '에서 접속되었습니다.')
 
-data = connectionSock.recv(1024)
-print('받은 데이터 : ', data.decode('utf-8'))
+sender = threading.Thread(target=send, args=(connectionSock,))
+receiver = threading.Thread(target=receive, args=(connectionSock,))
 
-connectionSock.send('I am a server.'.encode('utf-8'))
-print('메시지를 보냈습니다.')
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
